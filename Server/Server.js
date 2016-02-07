087746e6ade2e5034d8b2fddfd78bca0f12f8001
Matthew Daniel <mzdsocial@gmail.com>
2016-02-06 23:37:48 -0500

    Moved Server.js to scripts folder

--- Server/Server.js
+++ Server/Server.js
@@ -1,16 +1,32 @@
+// Begin HTTP server
 var http = require("http");
+var url = require("url");
+
 
 http.createServer(function (request, response) { //here, createServer(onRequest) is taking an anonymous function (a function defined in the parameter)
+    var queryData = url.parse(request.url, true).query;
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
+    
+    var retrieveData = function(queryData.Occasion, queryData.Temperature) {
+        this.Occasion = queryData.Occasion,
+        this.Temperature = queryData.Temperature
+    };
+    
+    
     response.end();
-}).listen(8888);
+});
+
+
+
+//Begin Express
+
+// Begin Decision Tree
 
 var DecisionTree = require('decision-tree');
 
 var training_data = [
-   /* {"1Type": 0, "1Subtype": 0, "1Color": 0, "1Pattern": 0, "1Occassion": 0, "1Temperature": 0, "1Body": 0, "2Type": 0, "2Subtype": 0, "2Color": 0, "2Pattern": 0, "2Occassion": 0, "2Temperature": 0, "2Body": 0, "3Type": 0, "3Subtype": 0, "3Color": 0, "3Pattern": 0, "3Occassion": 0, "3Temperature": 0, "3Body": 0, "4Type": 0, "4Subtype": 0, "4Color": 0, "4Pattern": 0, "4Occassion": 0, "4Temperature": 0, "4Body": 0, "5Type": 0, "5Subtype": 0, "5Color": 0, "5Pattern": 0, "5Occassion": 0, "5Temperature": 0, "5Body": 0, "Accept": 0},*/
-    
+   /* {"1Type": 0, "1Subtype": 0, "1Color": 0, "1Pattern": 0, "1Occassion": 0, "1Temperature": 0, "1Body": 0, "2Type": 0, "2Subtype": 0, "2Color": 0, "2Pattern": 0, "2Occassion": 0, "2Temperature": 0, "2Body": 0, "3Type": 0, "3Subtype": 0, "3Color": 0, "3Pattern": 0, "3Occassion": 0, "3Temperature": 0, "3Body": 0, "4Type": 0, "4Subtype": 0, "4Color": 0, "4Pattern": 0, "4Occassion": 0, "4Temperature": 0, "4Body": 0, "5Type": 0, "5Subtype": 0, "5Color": 0, "5Pattern": 0, "5Occassion": 0, "5Temperature": 0, "5Body": 0, "Accept": 0},*/ 
     
     {"1_Type": "Outerwear","1_Subtype": "Jacket","1_Color": "Black","1_Pattern": "Down","1_Occassion": "3","1_Temperature": "5","1_Body": "2","2_Type": "Sweater","2_Subtype": "Medium","2_Color": "Grey","2_Pattern": "No","2_Occassion": "3","2_Temperature": "4","2_Body": "2","3_Type": "Shirt","3_Subtype": "Dress","3_Color": "Maroon","3_Pattern": "No","3_Occassion": "4","3_Temperature": "0","3_Body": "2","4_Type": "Pants","4_Subtype": "Chino","4_Color": "Green","4_Pattern": "No","4_Occassion": "3","4_Temperature": "2","4_Body": "3","5_Type": "Shoes","5_Subtype": "Boots","5_Color": "Brown","5_Pattern": "Leather","5_Occassion": "3","5_Temperature": "4","5_Body": "4","accepted": true},
 {"1_Type": "Outerwear","1_Subtype": "Jacket","1_Color": "Grey","1_Pattern": "Waterproof","1_Occassion": "3","1_Temperature": "6","1_Body": "2","2_Type": "Sweater","2_Subtype": "Medium","2_Color": "Blue","2_Pattern": "Yes","2_Occassion": "2","2_Temperature": "4","2_Body": "2","3_Type": "Shirt","3_Subtype": "Polo","3_Color": "Purple","3_Pattern": "No","3_Occassion": "Any","3_Temperature": "1","3_Body": "2","4_Type": "Pants","4_Subtype": "Jeans","4_Color": "Grey","4_Pattern": "No","4_Occassion": "3","4_Temperature": "2","4_Body": "3","5_Type": "Shoes","5_Subtype": "Skate","5_Color": "Red","5_Pattern": "Canvas","5_Occassion": "2","5_Temperature": "1","5_Body": "4","accepted": false},
@@ -28,26 +44,7 @@ var dt = new DecisionTree(training_data, class_name, features);
 
 var predicted_class = dt.predict(
     {"1_Type": "Outerwear","1_Subtype": "Jacket","1_Color": "Grey","1_Pattern": "Waterproof","1_Occassion": "3","1_Temperature": "6","1_Body": "2","2_Type": "Sweater","2_Subtype": "Light","2_Color": "Green","2_Pattern": "No","2_Occassion": "4","2_Temperature": "3","2_Body": "2","3_Type": "Shirt","3_Subtype": "Dress","3_Color": "Maroon","3_Pattern": "No","3_Occassion": "4","3_Temperature": "0","3_Body": "2","4_Type": "Pants","4_Subtype": "Chino","4_Color": "Green","4_Pattern": "No","4_Occassion": "3","4_Temperature": "2","4_Body": "3","5_Type": "Shoes","5_Subtype": "Athletic","5_Color": "Black","5_Pattern": "Yes","5_Occassion": "1","5_Temperature": "1","5_Body": "4"}
-    
-   /* {"1Type": "Outerwear", "1Subtype": "Jacket", "1Color": "Grey", "1Pattern": "Waterproof", "1Occassion": 3, "1Temperature": 4, "1Body": 2, "2Type": 0, "2Subtype": 0, "2Color": 0, "2Pattern": 0, "2Occassion": 0, "2Temperature": 0, "2Body": 0, "3Type": "Shirt", "3Subtype": "Polo", "3Color": "Black", "3Pattern": "Solid", "3Occassion": "Any", "3Temperature": 1, "3Body": 2, "4Type": "Pants", "4Subtype": "Shorts", "4Color": "Blue", "4Pattern": "Checkered", "4Occassion": 1, "4Temperature": 1, "4Body": 3, "5Type": "Shoes", "5Subtype": "Skate", "5Color": "Red", "5Pattern": "Canvas", "5Occassion": 2, "5Temperature": 3, "5Body": 4}*/
-    
 );
 
 console.log("");
 console.log(predicted_class);
-
-    /*
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"},
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "White", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"},
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Dark Blue", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"},
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Light", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Heavy", "Color": "Black", "Pattern": "Aztec", "Occassion": "2", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Sweater Jacket", "Color": "Grey", "Pattern": "Solid", "Occassion": "4", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Medium", "Color": "Grey", "Pattern": "Solid", "Occassion": "2", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Medium", "Color": "Grey", "Pattern": "Solid", "Occassion": "4", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Medium", "Color": "Green", "Pattern": "Solid", "Occassion": "4", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Medium", "Color": "Dark Blue", "Pattern": "Solid", "Occassion": "2", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Medium", "Color": "Yellow", "Pattern": "Solid", "Occassion": "4", "Temperature": "4", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Light", "Color": "Orange", "Pattern": "Solid", "Occassion": "3", "Temperature": "3", "Body": "2"},
-    {"Type": "Sweater", "Subtype": "Light", "Color": "Dark Blue", "Pattern": "Solid", "Occassion": "2", "Temperature": "4", "Body": "2"},
-    */
