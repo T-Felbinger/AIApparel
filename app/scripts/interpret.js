ab0fb07d9ed1de748e7492f3b7640b5cfe2b7227
David Thames <davidc.thames@gmail.com>
2016-02-07 03:02:07 -0500

    Stuff
    - Added jquery to app
    - completed interpret server data

--- app/scripts/interpret.js
+++ app/scripts/interpret.js
@@ -1,21 +1,38 @@
 function request() {
-    var books= []; // Array that will contain all books
-    $(document).ready(function() {
-        // http request
-           $.getJSON("localhost/?occ=" + $("paper-menu#occassion").prop("selected") + "&temp=3", function(data) {
-              $.each(data, function(i, val){
-             books.push(val);
-              });
-        alert(books) ;
-            });
-    });
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET","localhost:8000/?Occasion=" + ($("paper-menu#occasion").prop("selected") + 1) + "&temp=3", true);
+    xmlhttp.onreadystatechange=function(){
+         if (xmlhttp.readyState==4 && xmlhttp.status==200){
+           string=xmlhttp.responseText;
+         }
+   }
+   xmlhttp.send();
 }
 
 
-function interpret(output) {
-    var myobj = JSON.parse('{ "hello":"world" }');
+function refresh() {
     
-    console.log(myobj.hello);
+    var outfit = JSON.parse(request());
+    
+    $("#Outerwear #Subtype").html("outfit.1_Subtype");
+    $("#Outerwear #Color").html("outfit.1_Color");
+    $("#Outerwear #Pattern").html("outfit.1_Pattern");
+    
+    $("#Sweater #Subtype").html("outfit.2_Subtype");
+    $("#Sweater #Color").html("outfit.2_Color");
+    $("#Sweater #Pattern").html("outfit.2_Pattern");
+    
+    $("#Shirt #Subtype").html("outfit.3_Subtype");
+    $("#Shirt #Color").html("outfit.3_Color");
+    $("#Shirt #Pattern").html("outfit.3_Pattern");
+    
+    $("#Pants #Subtype").html("outfit.4_Subtype");
+    $("#Pants #Color").html("outfit.4_Color");
+    $("#Pants #Pattern").html("outfit.4_Pattern");
+    
+    $("#Shoes #Subtype").html("outfit.5_Subtype");
+    $("#Shoes #Color").html("outfit.5_Color");
+    $("#Shoes #Pattern").html("outfit.5_Pattern");
     
 }
 interpret("a");
