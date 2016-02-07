29df5f2e1986c4deb1bdbdf9bd51c7a009f070f6
David Thames <davidc.thames@gmail.com>
2016-02-06 23:26:01 -0500

    Finished Guessing

--- app/scripts/guessing.js
+++ app/scripts/guessing.js
@@ -1,18 +1,79 @@
 var shirts = [
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"};
-]
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Dark Grey", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Light Grey", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "White", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Dark Grey", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Purple", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Dark Blue", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Light Blue", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Light Blue", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Turquoise", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Lime Green", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Orange", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Polo", "Color": "Teal", "Pattern": "No", "Occassion": "3", "Temperature": "1", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Black", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Black", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Light Blue", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Blue", "Pattern": "Pinstripe", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Dark Blue", "Pattern": "Pinstripe", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Blue", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "White", "Pattern": "Yes", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Pink", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Maroon", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "White", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "White", "Pattern": "No", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "Blue", "Pattern": "Pinstripe", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Dress", "Color": "White", "Pattern": "Pinstripe", "Occassion": "4", "Temperature": "0", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "Grey", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "Brown", "Pattern": "Plaid", "Occassion": "3", "Temperature": "2", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "Blue", "Pattern": "Plaid", "Occassion": "3", "Temperature": "2", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "Maroon", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "White", "Pattern": "Flanel", "Occassion": "3", "Temperature": "2", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "Pink", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "2"},
+    {"Type": "Shirt", "Subtype": "Button Down", "Color": "Maroon", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "2"},
+];
 var outerwear = [
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"};
-]
+    {"Type": "Outerwear", "Subtype": "No", "Color": "No", "Pattern": "No", "Occassion": "No", "Temperature": "No", "Body": "No"},
+    {"Type": "Outerwear", "Subtype": "Jacket", "Color": "Black", "Pattern": "Down", "Occassion": "3", "Temperature": "5", "Body": "2"},
+    {"Type": "Outerwear", "Subtype": "Jacket", "Color": "Grey", "Pattern": "Waterproof", "Occassion": "3", "Temperature": "6", "Body": "2"},
+    {"Type": "Outerwear", "Subtype": "Jacket", "Color": "Grey", "Pattern": "Fleece", "Occassion": "3", "Temperature": "4", "Body": "2"},
+    {"Type": "Outerwear", "Subtype": "Jacket", "Color": "Grey", "Pattern": "Ski", "Occassion": "3", "Temperature": "6", "Body": "2"},
+    {"Type": "Outerwear", "Subtype": "Vest", "Color": "Grey", "Pattern": "Down", "Occassion": "3", "Temperature": "5", "Body": "2"},
+];
 var sweaters = [
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"};
-]
+    {"Type": "sweater", "Subtype": "No", "Color": "No", "Pattern": "No", "Occassion": "No", "Temperature": "No", "Body": "No"},
+    {"Type": "Sweater", "Subtype": "Heavy", "Color": "Black", "Pattern": "Yes", "Occassion": "3", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Heavy", "Color": "Dark Green", "Pattern": "No", "Occassion": "3", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Heavy", "Color": "Grey", "Pattern": "No", "Occassion": "4", "Temperature": "3", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Medium", "Color": "Grey", "Pattern": "No", "Occassion": "3", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Medium", "Color": "Grey", "Pattern": "No", "Occassion": "3", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Light", "Color": "Grey", "Pattern": "No", "Occassion": "3", "Temperature": "3", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Medium", "Color": "Green", "Pattern": "No", "Occassion": "3", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Medium", "Color": "Dark Blue", "Pattern": "No", "Occassion": "2", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Medium", "Color": "Blue", "Pattern": "Yes", "Occassion": "2", "Temperature": "4", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Light", "Color": "Yellow", "Pattern": "No", "Occassion": "4", "Temperature": "3", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Light", "Color": "Orange", "Pattern": "No", "Occassion": "3", "Temperature": "3", "Body": "2"},
+    {"Type": "Sweater", "Subtype": "Medium", "Color": "Dark Blue", "Pattern": "No", "Occassion": "3", "Temperature": "3", "Body": "2"},
+];
 var pants = [
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"};
-]
+    {"Type": "Pants", "Subtype": "Jeans", "Color": "Silver", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Jeans", "Color": "Grey", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Chino", "Color": "Khaki", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Chino", "Color": "Green", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Jeans", "Color": "Blue", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Shorts", "Color": "Navy", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Shorts", "Color": "White", "Pattern": "Plaid", "Occassion": "3", "Temperature": "2", "Body": "3"},
+    {"Type": "Pants", "Subtype": "Shorts", "Color": "Grey", "Pattern": "No", "Occassion": "3", "Temperature": "2", "Body": "3"},
+];
 var shoes = [
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"};
-]
+    {"Type": "Shoes", "Subtype": "Dress", "Color": "Black", "Pattern": "No", "Occassion": "5", "Temperature": "1", "Body": "4"},
+    {"Type": "Shoes", "Subtype": "Chuckas", "Color": "Blue", "Pattern": "Raw Leather", "Occassion": "3", "Temperature": "1", "Body": "4"},
+    {"Type": "Shoes", "Subtype": "Chuckas", "Color": "Grey", "Pattern": "Raw Leather", "Occassion": "3", "Temperature": "1", "Body": "4"},
+    {"Type": "Shoes", "Subtype": "Boots", "Color": "Brown", "Pattern": "Leather", "Occassion": "3", "Temperature": "4", "Body": "4"},
+    {"Type": "Shoes", "Subtype": "Athletic", "Color": "Black", "Pattern": "Yes", "Occassion": "1", "Temperature": "1", "Body": "4"},
+    {"Type": "Shoes", "Subtype": "Skate", "Color": "Red", "Pattern": "Canvas", "Occassion": "2", "Temperature": "1", "Body": "4"},
+];
 
     
 var UserCloset = {
@@ -21,19 +82,59 @@ var UserCloset = {
     sweater: sweaters,
     pants: pants,
     shoes: shoes    
+};
+
+function weighting(clothes, occasion, temp) {
+    if(clothes.Occassion == "No") 
+        return 1;
+    else
+        return 1 / ((Math.abs(occasion - clothes.Occassion) * Math.abs(temp - clothes.Temperature)) + 1);
 }
 
-function guess(closet, temp, occasion) {
-    var shirt = Math.round(Math.random * closet.shirts.length);
-    var sweater = Math.round(Math.random * closet.sweater.length);
-    var outerwear = Math.round(Math.random * closet.outerwear.length);
-    var pants = Math.round(Math.random * closet.pants.length);
-    var shoes = Math.round(Math.random * closet.pants.length);
+function guess(o, occasion, temp) {
+//    var shirt = Math.round(Math.random * closet.shirts.length);
+//    var sweater = Math.round(Math.random * closet.sweater.length);
+//    var outerwear = Math.round(Math.random * closet.outerwear.length);
+//    var pants = Math.round(Math.random * closet.pants.length);
+//    var shoes = Math.round(Math.random * closet.pants.length);
+//    
+    var outfit = {};
     
-    var outfit
+    
+    
+    var sum = {};
+    var weight = {};
+
+    for(var p in o){
+        sum[p] = 0;
+        for(i of o[p]) {
+            sum[p] += weighting(i, occasion, temp);
+        }
+    }
+    
+    var target = Math.random() * sum;
+    var searching = true;
+    var count = 0;
+    
+    for(var p in o) {
+        var target = Math.random() * sum[p];
+        count = 0;
+        searching = true;
+        for(i = 0; i < o[p].length; i++) {
+            count += weighting(o[p][i], occasion, temp);
+            if(count > target && searching) {
+                searching = false;
+                outfit[p] = o[p][i];
+                console.log(o[p][i]);
+            }
+        }
+    }
+    
+    
+    console.log(outfit);
 }
 
-guess(UserCloset, 30, 3);
+guess(UserCloset, 3, 3);
 
 //1shirt
 //2sweater
@@ -42,7 +143,7 @@ guess(UserCloset, 30, 3);
 //5shoes
 
 
-var = outfit = {"1Type": "Shirt", "1Subtype": "Polo", "1Color": "Black", "1Pattern": "Solid", "1Occassion": "Any", "1Temperature": "1", "1Body": "2", "1Type": "Shirt", "1Subtype": "Polo", "1Color": "Black", "1Pattern": "Solid", "1Occassion": "Any", "1Temperature": "1", "1Body": "2"};
-
-
-    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "Any", "Temperature": "1", "Body": "2"};
+//var outfit = {"1Type": "Shirt", "1Subtype": "Polo", "1Color": "Black", "1Pattern": "Solid", "1Occassion": "3", "1Temperature": "1", "1Body": "2", "1Type": "Shirt", "1Subtype": "Polo", "1Color": "Black", "1Pattern": "Solid", "1Occassion": "3", "1Temperature": "1", "1Body": "2"};
+//
+//
+//    {"Type": "Shirt", "Subtype": "Polo", "Color": "Black", "Pattern": "Solid", "Occassion": "3", "Temperature": "1", "Body": "2"};
